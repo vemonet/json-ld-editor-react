f7d8be64509a54bbbb40e291e2330aec37eb8556
vemonet <vincent.emonet@gmail.com>
2021-03-19 17:14:30 +0000

    Deploying to gh-pages from  @ c719b3268180d6aaaab1d591fb9fdb158217e58d 🚀

---
+++ precache-manifest.a9e92e8d59406bd45437e49c6c673031.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "dc12a57398e6d23d4923",
+    "url": "/fair-metadata-wizard/static/js/app.199cacce.chunk.js"
+  },
+  {
+    "revision": "89f3b6fc6cbe301bd73e",
+    "url": "/fair-metadata-wizard/static/js/runtime~app.256c1066.js"
+  },
+  {
+    "revision": "495b8282bd771d963be0",
+    "url": "/fair-metadata-wizard/static/js/2.f87c1ea3.chunk.js"
+  },
+  {
+    "revision": "951e6ef1c62c25b9b10b081968119584",
+    "url": "/fair-metadata-wizard/static/media/icon.72626586.png"
+  },
+  {
+    "revision": "70bf6ddc9195be0eeae81739c9efa8a1",
+    "url": "/fair-metadata-wizard/index.html"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/fair-metadata-wizard/serve.json"
+  },
+  {
+    "revision": "2002987acac9deb6803ab4ba381226ba",
+    "url": "/fair-metadata-wizard/expo-service-worker.js"
+  },
+  {
+    "revision": "e70094380a951690ba424fb454f2dc99",
+    "url": "/fair-metadata-wizard/register-service-worker.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/fair-metadata-wizard/static/js/2.f87c1ea3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e78221aba04ce077cb705ff3c8d2bc05",
+    "url": "/fair-metadata-wizard/favicon-16.png"
+  },
+  {
+    "revision": "6a52670e86212233b8d906cc16a59955",
+    "url": "/fair-metadata-wizard/favicon-32.png"
+  },
+  {
+    "revision": "1deb5f88e9bbfe62f2cd9913b9f9d46e",
+    "url": "/fair-metadata-wizard/favicon.ico"
+  },
+  {
+    "revision": "2c10bb42f4ee482f3949adf918c0055c",
+    "url": "/fair-metadata-wizard/manifest.json"
+  }
+];
---
+++ register-service-worker.js
@@ -0,0 +1,1 @@
+"serviceWorker"in navigator&&window.addEventListener("load",(function(){navigator.serviceWorker.register("/fair-metadata-wizard/expo-service-worker.js",{scope:"/fair-metadata-wizard/"}).then((function(e){})).catch((function(e){console.info("Failed to register service-worker",e)}))}));
