1baf055bd8a43d29c53248c8c9222e5bd84a8333
vemonet <vincent.emonet@gmail.com>
2021-03-22 08:17:38 +0000

    Deploying to gh-pages from  @ 98ca678c827f52b2ab24644383796567f8184de8 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/fair-metadata-wizard/precache-manifest.279ac14ff8450443e934f61ab86c3f1d.js"
+  "/fair-metadata-wizard/precache-manifest.b64fa22ea425fc766f1cb8036c56253a.js"
 );
 
 workbox.skipWaiting();
